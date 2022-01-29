814f0153fcf198c1309b9f15caf0eccda6a892fe
sistemasfinorleonel <ingenierosistemasfinor@gmail.com>
2022-01-29 11:18:28 -0400

    j

--- resources/views/livewire/empresa-ac/index.blade.php
+++ resources/views/livewire/empresa-ac/index.blade.php
@@ -1,15 +1,16 @@
 <div>
     <div class="card">
         <div class="card-body">
-            <select name="" id="" class="form-control" wire:modal="empresa_id">
+
+<form action="{{ {{ route('routeName') }}}}">            <select name="" id="" class="form-control" wire:modal="empresa_id">
                     <option value="">Selecione una empresas</option>            
                     @foreach (empresas_user(Auth::user()->id) as $empresa)
                         <option value="{{$empresa->idEmpresa}}">{{$empresa->idEmpresa}}{{$empresa->razonsocial}}</option>
                     @endforeach
             </select>
-            ijoij
+
             {{$this->empresa_id}}
         </div>
-        
+    </form>
     </div>
  </div>
